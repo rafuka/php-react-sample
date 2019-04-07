3df60af69fa92d492f631491f39021c42a538e16
rafuka <rafuk89@gmail.com>
2019-04-07 14:40:01 +0200

    created Error component

---
+++ src/components/error/Error.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Error extends Component {
+  render() {
+    return (
+      <div className="error">
+        <img className="error__image" src="./error-img.png" alt="Error" />
+        <h2>Woops! There's been an error :(</h2>
+        {this.props.message
+          ? (
+            <p className="error__message">{this.props.message}</p>
+          )
+          : null}
+      </div>
+    );
+  }
+}
+
+export default Error;
