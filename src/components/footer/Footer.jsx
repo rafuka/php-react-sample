274fe95390958529eb6e01b5be1cc5b81623d1a6
rafuka <rafuk89@gmail.com>
2019-04-07 16:48:22 +0200

    added footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './footer.scss';
+
+const Footer = props => {
+  return (
+    <footer className="footer">
+      <p className="footer__text">
+        Created by Rafael Garc&iacute;a
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
