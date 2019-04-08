8786280ea8eadc32fb007e3267b6f3d3b180ac38
rafuka <rafuk89@gmail.com>
2019-04-08 14:26:54 +0200

    added Modal component

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,9 +2,53 @@ import React, { Component } from 'react';
 import './modal.scss';
 
 class Modal extends Component {
-  state = {  }
-  render() { 
-    return ();
+  state = {
+    visible: false
+  }
+
+  handleToggle = (e) => {
+    e.preventDefault();
+    this.setState(
+      ({ visible }) => ({ visible: !visible }),
+      () => {
+        const { onToggle } = this.props;
+        if (onToggle) onToggle(this.state.visible)  
+      }
+    );
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Modal.Toggler handleToggle={this.handleToggle}>
+          {this.props.toggler}
+        </Modal.Toggler>
+        <Modal.Body visible={this.state.visible}>
+          <Modal.Toggler handleToggle={this.handleToggle}>
+            {({toggle}) => <button onClick={toggle} className="modal__close">X</button>}
+          </Modal.Toggler>
+          {this.props.children}
+        </Modal.Body>
+      </React.Fragment>
+    );
+  }
+
+  static Toggler = props => { // All the children of this element will act as togglers on click
+    return props.children({
+      toggle: props.handleToggle
+    })
+  }
+
+  static Body = props => {
+    const { visible } = props;
+    let modalVisibleClass = visible ? ' visible' : '';
+    return (
+      <div className={"modal" + modalVisibleClass}>
+        <div className="modal__container">   
+          {props.children}
+        </div>
+      </div>
+    );
   }
 }
  
